eefa16d12c0f3c3f018a4d62aa0e585200ac2da8
Aaron Smith <smitham50@gmail.com>
2019-09-26 16:43:14 -0400

    added demo user and updated create method and user model

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,20 +2,27 @@ class Api::V1::UsersController < ApplicationController
 
   def index
     users = User.all
-
     render json: users
   end
 
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
   def create
-    user = User.create(
-      name: params[:name], 
+    user = User.new(
+       name: params[:name], 
       password: params[:password], 
       email: params[:email], 
       picture: params[:picture], 
       karma_credits: params[:karma_credits], 
       about: params[:about])
-
-    render json: user
+    if user.save
+      render json: user
+    else
+      render json: {errors: user.errors.full_messages}
+    end
   end
 
 end
